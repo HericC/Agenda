74a441065e9f17fd1d308210eaf13dbb37b62e7c
Heric Macedo Oliveira Silva <heric_dev@hotmail.com>
2021-08-09 10:33:50 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    // mode: 'production',
+    entry: './frontend/index.js',
+    devtool: 'source-map',
+    // devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    configFile: 'tsconfig.frontend.json',
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    },
+};
